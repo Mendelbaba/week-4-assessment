ff6d18612b6133bd494beb65b64fd48043ecf172
Mendelbaba <mendybaba@gmail.com>
2021-11-05 16:59:21 -0400

    done for friday will continue saturday night

--- server/index.js
+++ server/index.js
@@ -22,4 +22,38 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/fortunes", (req, res) => {
+  const fortunes = ["A beautiful, smart, and loving person will be coming into your life.",
+    "A dubious friend may be an enemy in camouflage.",
+    "A faithful friend is a strong defense.",
+    "A feather in the hand is better than a bird in the air.",
+    "A fresh start will put you on your way.",
+
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+
+})
+
+app.get("/api/malaphors", (req, res) => {
+  const malaphors = ["You opened this can of worms, now you gotta sleep in it.",
+    "We will burn that bridge when we get there.",
+    "There's more than one way to beat a dead horse",
+    "Dont count your blessings before they hatch.",
+    "If it ain’t rocket surgery, don’t fix it.",
+    "You can kill two birds in the hand with one stone in a bush",
+    "	He doesn't have a leg to stand on or a window to throw it out of."
+
+  ];
+
+  let randomIndex = Math.floor(Math.random() * malaphors.length);
+  let randomMalaphor = malaphors[randomIndex];
+  
+  res.status(200).send(randomMalaphor);
+
+})
+
 app.listen(4000, () => console.log("Server running on 4000"));
